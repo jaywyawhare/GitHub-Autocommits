9c074b801665243f26cb55574a39e8c5b37b5d51
jay <2005441@kiit.ac.in>
2022-03-18 13:08:19 +0530

    auto commit

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-for (( ; ; ))
+for (( i=1; i<70; i++ ))
 do
-
     if [ -f test.txt ]
     then
         rm test.txt
@@ -15,3 +14,21 @@ do
     git push
     sleep 1
 done
+
+# for (( ; ; ))
+# do
+#
+#    if [ -f test.txt ]
+#    then
+#        rm test.txt
+#    else
+#        touch test.txt
+#    fi
+#
+#    git add .
+#    git commit -m "auto commit"
+#    git push
+#    sleep 1
+# done
+
+
